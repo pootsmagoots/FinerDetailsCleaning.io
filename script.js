eecee3a1d9fb8f24191906fb1c40f73ed3825981
Travis Batts <Travisbatts6@gmail.com>
2017-05-03 13:50:35 -0400

    changes made to contact page

--- script.js
+++ script.js
@@ -1,22 +1,6 @@
 $( document ).ready(function(){
 
-// $("#residential").hover(function(){
-//   $(this).html("ResidentialServices")
-//
-// })
 
-
-$( "#residential" ).click(function() {
-  alert( "Handler for .click() called." );
-});
-
-
-// 
-// $(function(){
-//     $(".flip").flip({
-//         trigger: 'hover'
-//     });
-// });
 
 
 })
